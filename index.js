6c79b6c089ae2e713a176eeacf019d0f6b7af844
VENKYS <vs1420100851@gmail.com>
2022-03-12 15:27:39 +0000

    commit-1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const app = express();
+
+const dbPath = path.join(__dirname, "gooreads.db");
+let db = null;
+const initializeDBAndStartServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DB ERROR: ${e.message}`);
+  }
+};
+initializeDBAndStartServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+      book
+    ORDER BY
+      book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
